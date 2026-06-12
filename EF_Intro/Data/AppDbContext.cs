992eec6acfa7d2f4abcee69cfb19732f8093e05b
dipak921 <dipakchande921@gmail.com>
2026-06-12 13:17:06 +0530

    Add project files.

---
+++ EF_Intro/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EF_Intro.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Models.Student> Students { get; set; }
+    }
+}
---
+++ EF_Intro/Models/Student.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EF_Intro.Models
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Column("StudentName", TypeName = "varchar(50)")]
+        public string Name { get; set; }
+
+
+        [Required]
+        public string Gender { get; set; }
+
+        [Required]
+        public int Age { get; set; }
+    }
+}
